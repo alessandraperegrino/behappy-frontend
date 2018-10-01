645f94c088be7b01ddb30fcc42390fe8769f0808
alessandraperegrino <alessandraperegrino@github.com>
2018-10-01 16:07:39 -0300

    parei no arquivo : ./src/App.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Header from './Header';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+export default Header;
